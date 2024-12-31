302da656688f5b292c3c4847e29c217d5eaa547d
Dumuthu Jayasinghe <126330563+Dumuthu2000@users.noreply.github.com>
2024-12-31 22:39:40 +0530

    Update all features

---
+++ screens/LightAutomation.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const LightAutomation = () => {
+  return (
+    <View>
+      <Text>LightAutomation</Text>
+    </View>
+  )
+}
+
+export default LightAutomation
+
+const styles = StyleSheet.create({})
---
+++ screens/SoilMositureControl.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const SoilMositureControl = () => {
+  return (
+    <View>
+      <Text>SoilMositureControl</Text>
+    </View>
+  )
+}
+
+export default SoilMositureControl
+
+const styles = StyleSheet.create({})
